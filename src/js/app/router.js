0eb8aa2553f472f36c04df0671a3788634fb7cb3
Colin Ferm <colin@fortytwosolutions.com>
2022-03-23 17:45:52 +0100

    Shelled out routes

--- src/js/app/router.js
+++ src/js/app/router.js
@@ -5,14 +5,34 @@ BBLM.Router = Backbone.Router.extend({
 
 	routes:{
 		"":"listRaces",
+		"login": "login",
+		"logout": "logout",
 		"setup": "setupDashboard",
 		"race":"listRaces",
 		"race/new": "raceForm",
-		"race/:id": "raceForm"
+		"race/:id": "raceForm",
+		"leagues": "showLeagues",
+		"leage/new": "leagueCreate",
+		"league/:id": 'leagueDivisionList',
+		"division/new": "divisionCreate",
+		"division/:id": "divisionEdit",
+		"team/new": "teamCreate",
+		"team/:id": "teamDisplay",
+		"season/new": "seasonCreate",
+		"season/:id": "displaySchedule",
+		"logGame/:id": "logGame"
 	},
 
 	initialize: function() {
 		//this.topNav = 
+	},
+
+	login: function() {
+
+	},
+
+	logout: function() {
+
 	},
 
 	setupDashboard: function() {
@@ -30,5 +50,41 @@ BBLM.Router = Backbone.Router.extend({
 
 	raceForm: function(id) {
 
+	},
+
+	showLeagues: function() {
+
+	},
+
+	leagueDivisionList: function(id) {
+
+	},
+
+	divisionCreate: function() {
+
+	},
+
+	divisionEdit: function(id) {
+
+	},
+
+	teamCreate: function() {
+
+	},
+
+	teamDisplay: function(id) {
+
+	},
+
+	seasonCreate: function() {
+
+	},
+
+	displaySchedule: function(id) {
+
+	},
+
+	logGame: function(id) {
+		
 	}
 });
